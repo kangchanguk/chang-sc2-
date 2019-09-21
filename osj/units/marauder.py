0f79794290551ab56b46bf288e7884750af2f45d
kangchanguk <rkdckddnr01@naver.com>
2019-09-21 23:50:49 +0900

    delete

--- osj/units/marauder.py
+++
@@ -1,8 +0,0 @@
-import sc2
-from sc2.ids.unit_typeid import UnitTypeId
-from . import unitwrapper
-
-class Marauder(unitwrapper.UnitWrapper):
-    def __init__(self, tag, army):
-        super().__init__(tag, army)
-        self.priority += [UnitTypeId.MARAUDER, UnitTypeId.AUTOTURRET]
--- osj/units/marine.py
+++
@@ -1,8 +0,0 @@
-import sc2
-from sc2.ids.unit_typeid import UnitTypeId
-from . import unitwrapper
-
-class Marine(unitwrapper.UnitWrapper):
-    def __init__(self, tag, army):
-        super().__init__(tag, army)
-        self.priority.append(UnitTypeId.MARINE)
